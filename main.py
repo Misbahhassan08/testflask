e7f4c5bd34949a3c7de7edde63251b1197608d64
misbahhassan08 <misbahhassan08@gmail.com>
2024-10-21 16:55:09 +0500

    update main.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from flask import Flask
-from dotenv import load_dotenv
 import os
 from flask_cors import CORS
-load_dotenv
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 CORS(app)
-app.config['DEBUG'] = os.environ.get('FLASK_DEBUG')
+app.app_context().push()
+
+
 
 @app.route('/')
 def hello_world():
     return "hello , world"
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(port=8080, debug=True, use_reloader=False)
+    #app.run(host='0.0.0.0', port=port)
