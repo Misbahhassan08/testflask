2a18599bc589e58c6f7a464e2732f4a14c637e57
misbahhassan08 <misbahhassan08@gmail.com>
2024-10-21 16:06:16 +0500

    changing app file

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+from flask_cors import CORS
+load_dotenv
+
+app = Flask(__name__)
+CORS(app)
+app.config['DEBUG'] = os.environ.get('FLASK_DEBUG')
+
+@app.route('/')
+def hello_world():
+    return "hello , world"
+if __name__ == '__main__':
+    app.run()
