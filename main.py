4007a4301dab96b593d5ce60a85542ee37844489
misbahhassan08 <misbahhassan08@gmail.com>
2024-10-23 16:59:27 +0500

    add object removal code

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from flask import Flask, request, send_file, jsonify
 from flask_cors import CORS
+
+import gc
 import os
+import torch
+import base64
+from io import BytesIO
+import torch.nn.functional as F
+import torchvision as tv
+from objectRemoval_engine import SimpleLama
 from backgroundremover import utilities
 from backgroundremover.bg import remove
+from projectUtils import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 CORS(app)
 app.app_context().push()
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+simple_lama = SimpleLama(device=device)
 # -------------------------------------------- ------------------ -----------------------------------------
 # -------------------------------------------- index ------------------------------------------------------
 # -------------------------------------------- ------------------ -----------------------------------------
@@ -58,6 +71,57 @@ def rgb():
     
     print(f" BG-image base 64 = {new_image[20:]}")
     return jsonify({"bg_image":new_image})
+
+
+#------------------------------------ Object removal ---------------------------------------------
+
+"""
+[{'startX': 1033.9316, 'startY': 1210.915, 'endX': 1033.9316, 'endY': 1267.9419, 'strokeWidth': 20}, {'startX': 1033.9316, 'startY': 1267.9419, 'endX': 1033.9316, 'endY': 1272.8931, 'strokeWidth': 20}, {'startX': 1033.9316, 'startY': 1272.8931, 'endX': 1033.9316, 'endY': 1288.873, 'strokeWidth': 20}, {'startX': 1033.9316, 'startY': 1288.873, 'endX': 1033.9316, 'endY': 1293.9355, 'strokeWidth': 20}]
+"""
+
+@app.route('/removeobj', methods=['POST'])
+def object_removal():
+    print("/REMOVE_OBJ new request coming")
+    data = request.get_json()
+    base64Image= data["image"]
+    base64mask= data["mask"]
+    size = data["size"]
+
+    print(f"Size found : {size}")
+
+    cv_img = base64toopencv(base64Image)
+    cv_mask = base64toopencv(base64mask)
+    cv2.imwrite("test.png",cv_img)
+    h, w, c = cv_img.shape
+    cv_mask = cv2.resize(cv_mask, (w, h))
+    cv2.imwrite("test_mask.png",cv_mask)
+
+
+    cv_mask = cv2.cvtColor(cv_mask, cv2.COLOR_BGR2RGB) 
+    cv_mask = Image.fromarray(cv_mask).convert('L') 
+
+    cv_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB) 
+    cv_img = Image.fromarray(cv_img) 
+
+    result = simple_lama(cv_img, cv_mask)
+    new_image = result
+    bio = io.BytesIO()
+    new_image.save(bio, "PNG")
+    bio.seek(0)
+    im_b64 = base64.b64encode(bio.getvalue()).decode()
+
+    return jsonify({"bg_image":im_b64}) # end of function end point removeobj 
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
